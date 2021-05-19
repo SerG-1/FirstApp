7668e4586a522c670681e61e7af4cfe96a04535d
Serejka <serg52rus@ya.ru>
2021-05-19 15:42:27 +0300

    Lvl1

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button;
+    private TextView text;
+    private Switch switch1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        text =findViewById(R.id.text);
+        switch1 =findViewById(R.id.switch1);
+        button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        button.setVisibility(View.GONE);
+        text.setText("WELCOME TO MYAPP!");
+        text.setTextSize(16);
+        switch1.setVisibility(View.VISIBLE);
+switch1.
+
     }
 }
